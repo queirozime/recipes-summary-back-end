dcfe02b99fee32ed643089ae8050ad097510bf8a
Rafael Pontes <90840183+rpontestl@users.noreply.github.com>
2023-06-04 21:17:06 -0300

    remove dist

--- dist/firestore/firestore.providers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const FirestoreDatabaseProvider = "firestoredb";
-export declare const FirestoreOptionsProvider = "firestoreOptions";
-export declare const FirestoreCollectionProviders: string[];
