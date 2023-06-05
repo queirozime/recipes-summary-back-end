364eff41db51b950a8059dd199d9daee5f3298b8
queirozime <carloshenricco@gmail.com>
2023-06-05 19:31:28 -0300

    add token in some requests

--- src/shoplists/shoplists.service.ts
+++ src/shoplists/shoplists.service.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { CreateShoplistDto } from 'src/shoplists/dto/create-shoplist.dto';
-import { UpdateShoplistDto } from 'src/shoplists/dto/update-shoplist.dto';
-import { ShoplistDocument } from 'src/shoplists/documents/shoplist.document';
-import { Shoplist } from 'src/shoplists/entities/shoplist.entity';
-import { AddRecipeDto } from './dto/add-recipe.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateShoplistDto } from "src/shoplists/dto/create-shoplist.dto";
+import { UpdateShoplistDto } from "src/shoplists/dto/update-shoplist.dto";
+import { ShoplistDocument } from "src/shoplists/documents/shoplist.document";
+import { Shoplist } from "src/shoplists/entities/shoplist.entity";
+import { AddRecipeDto } from "./dto/add-recipe.dto";
+import { AuthService } from "src/firestore/auth.service";
 
 @Injectable()
 export class ShoplistsService {
-  constructor(private readonly shoplistDocument: ShoplistDocument) {}
+  constructor(
+    private readonly shoplistDocument: ShoplistDocument,
+    private authService: AuthService
+  ) {}
 
-  create(createShoplistDto: CreateShoplistDto): Promise<Shoplist> {
+  async create(createShoplistDto: CreateShoplistDto): Promise<Shoplist> {
+    const userId = await this.authService.verifyTokenAndReturnUid(
+      createShoplistDto.token
+    );
     const shoplist = new Shoplist(
-      createShoplistDto.userId, 
-      createShoplistDto.title, 
-      createShoplistDto.favorite, 
+      userId,
+      createShoplistDto.title,
+      createShoplistDto.favorite,
       createShoplistDto.recipes
     );
     return this.shoplistDocument.create(shoplist);
